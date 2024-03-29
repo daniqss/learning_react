694564d347aeae7a17d1e7349e0b63a25535253b
ranicocs <danielqueijo14@gmail.com>
2024-03-29 14:36:28 +0100

    Usable edit modal menu

--- todos/src/components/Todo.jsx
+++ todos/src/components/Todo.jsx
@@ -1,31 +1,63 @@
-import PropTypes from 'prop-types';
-import TodoButton from './TodoButton';
-import './Todo.css';
+import { useState } from "react";
+import PropTypes from "prop-types";
+import TodoButton from "./TodoButton";
+import TodoModal from "./TodoModal"; // Importa el componente TodoModal
+import "./Todo.css";
 
-export default function Todo({ todo, handleCheck, handleEdit, handleRemove}) {
-    
-    return (
-        <section className='todo-card'>
-            <div className='right-side'>
-                <input 
-                    type='checkbox' 
-                    defaultChecked={todo.completed} 
-                    className='completedCheck' 
-                    onClick={() => handleCheck({...todo, completed: !todo.completed})}
-                />
-                <p>{todo.title}</p>
-            </div>
-            <div>
-                <TodoButton buttonStyle='edit-button' handleClick={handleEdit}>Edit</TodoButton>
-                <TodoButton buttonStyle='remove-button' handleClick={handleRemove}>Delete</TodoButton>
-            </div>
-        </section>
-    )
+export default function Todo({ todo, handleCheck, handleEdit, handleRemove }) {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+      setModal(!modal);
+  };
+
+  if(modal) {
+      document.body.classList.add('active-modal')
+  } else {
+      document.body.classList.remove('active-modal')
+  }
+
+  const handleEditClick = () => {
+    toggleModal()
+  }
+
+  return (
+    <>
+      <section className="todo-card">
+        <div className="right-side">
+          <input
+            type="checkbox"
+            defaultChecked={todo.completed}
+            className="completedCheck"
+            onClick={() => handleCheck({ ...todo, completed: !todo.completed })}
+          />
+          <p>{todo.title}</p>
+        </div>
+        <div>
+          <TodoButton buttonStyle="edit-button" handleClick={handleEditClick}>
+            Edit
+          </TodoButton>
+          <TodoButton buttonStyle="remove-button" handleClick={handleRemove}>
+            Delete
+          </TodoButton>
+        </div>
+      </section>
+      {modal && (
+        <TodoModal 
+          todo={todo}
+          confirmEdit={handleEdit}
+          handleClose={toggleModal}
+        >
+          
+        </TodoModal>
+      )}
+    </>
+  );
 }
 
 Todo.propTypes = {
-    todo: PropTypes.object.isRequired,
-    handleCheck: PropTypes.func.isRequired,
-    handleEdit: PropTypes.func.isRequired,
-    handleRemove: PropTypes.func.isRequired
-}
+  todo: PropTypes.object.isRequired,
+  handleCheck: PropTypes.func.isRequired,
+  handleEdit: PropTypes.func.isRequired,
+  handleRemove: PropTypes.func.isRequired,
+};
--- todos/src/components/TodoModal.jsx
+++ todos/src/components/TodoModal.jsx
@@ -1,29 +1,42 @@
-import PropTypes from 'prop-types';
+import { useState } from 'react'
+import PropTypes from 'prop-types'
 import "./TodoModal.css";
+import TodoButton from './TodoButton'
 
-export default function TodoModal({ children, onClose, show }) {
-  const handleClose = e => {
-    onClose && onClose(e)
-  }
+export default function TodoModal({children, todo, confirmEdit, handleClose }) {
+  const [newTitle, setNewTitle] = useState(todo.title);
 
-  if (!show) {
-    return null;
-  }
   return (
     <div className="modal">
-      <h2>Modal Window</h2>
-      <div className="content">{children}</div>
-      <div className="actions">
-        <button className="toggle-button" onClick={handleClose}>
-          close
-        </button>
-      </div>
+      <div onClick={handleClose} className="overlay"></div>
+          <div className="modal-content">
+            <h2>{todo.title}</h2>
+            <div>
+              {children}
+            </div>
+            <input type="text" defaultValue={newTitle} onChange={(e) => setNewTitle(e.target.value)}/>
+            <div>
+              <TodoButton buttonStyle="add-button" handleClick={
+                  () => {
+                    confirmEdit({ ...todo, title: newTitle })
+                    handleClose()
+                  }
+                }
+              >
+                Save
+              </TodoButton>
+              <TodoButton buttonStyle="remove-button" handleClick={handleClose}>
+                Cancel
+              </TodoButton>
+            </div>
+          </div>
     </div>
   )
 }
 
 TodoModal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  show: PropTypes.bool.isRequired,
-  children: PropTypes.node
+  children: PropTypes.node,
+  todo: PropTypes.object.isRequired,
+  confirmEdit: PropTypes.func.isRequired,
+  handleClose: PropTypes.func.isRequired
 }
