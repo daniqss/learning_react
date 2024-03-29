aef0d02f223948bd0e9956fff34f6362953a2318
ranicocs <danielqueijo14@gmail.com>
2024-03-29 01:58:00 +0100

    Style TodoList

--- todos/src/components/TodoList.jsx
+++ todos/src/components/TodoList.jsx
@@ -1,36 +1,41 @@
 import PropTypes from "prop-types";
 import Todo from "./Todo.jsx";
-// import "./TodoList.css";
+import "./TodoList.css";
 
 export default function TodoList({ todos, editTodo, removeTodo }) {
   return (
     <>
+      <h2>Active</h2>
       <section>
         {todos.map((todo) => {
           if (!todo.completed) {
             return (
-              <Todo
-                key={todo.id}
-                todo={todo}
-                handleCheck={(updatedTodo) => editTodo(todo.id, updatedTodo)}
-                handleEdit={(updatedTodo) => editTodo(todo.id, updatedTodo)}
-                handleRemove={() => removeTodo(todo.id)}
-              />
+              <div key={todo.id} className="todo">
+                <Todo
+                  todo={todo}
+                  handleCheck={(updatedTodo) => editTodo(todo.id, updatedTodo)}
+                  handleEdit={(updatedTodo) => editTodo(todo.id, updatedTodo)}
+                  handleRemove={() => removeTodo(todo.id)}
+                />
+              </div>
             );
           }
         })}
       </section>
+      <h2>Completed</h2>
       <section>
         {todos.map((todo) => {
           if (todo.completed) {
             return (
-              <Todo
-                key={todo.id}
-                todo={todo}
-                handleCheck={(updatedTodo) => editTodo(todo.id, updatedTodo)}
-                handleEdit={(updatedTodo) => editTodo(todo.id, updatedTodo)}
-                handleRemove={() => removeTodo(todo.id)}
-              />
+              <div key={todo.id} className="todo">
+                <Todo
+                  key={todo.id}
+                  todo={todo}
+                  handleCheck={(updatedTodo) => editTodo(todo.id, updatedTodo)}
+                  handleEdit={(updatedTodo) => editTodo(todo.id, updatedTodo)}
+                  handleRemove={() => removeTodo(todo.id)}
+                />
+              </div>
             );
           }
         })}
